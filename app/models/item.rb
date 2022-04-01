c06f3edd1cbd23913fdd4483e0bcc1c21e26206f
cut2key <k24lout@gmail.com>
2022-04-01 15:46:50 +0900

    add active hash content

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,17 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :quality
+  belongs_to :ship_fee
+  belongs_to :country
+  belongs_to :ship_day
 
   validates :item_name, presence: true
   validates :item_description, presence: true
   validates :category_id, numericality: { other_than: 1 }
-  validates :quality_id, presence: true
-  validates :ship_fee_id, presence: true
-  validates :country_id, presence: true
-  validates :ship_day_id, presence: true
+  validates :quality_id, numericality: { other_than: 1 }
+  validates :ship_fee_id, numericality: { other_than: 1 }
+  validates :country_id, numericality: { other_than: 0 }
+  validates :ship_day_id, numericality: { other_than: 1 }
   validates :price, presence: true
   #validates :user, presence: true
 
