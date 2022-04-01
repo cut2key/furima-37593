d5f0f93c43016c39731604e809f756d301d1256e
cut2key <k24lout@gmail.com>
2022-04-01 11:57:56 +0900

    add category info

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :quality
 
   validates :item_name, presence: true
   validates :item_description, presence: true
-  validates :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1 }
   validates :quality_id, presence: true
   validates :ship_fee_id, presence: true
   validates :country_id, presence: true
@@ -12,4 +15,5 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
 end
