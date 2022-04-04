f00b5b76a40b070de77ab33fdec137ea31a51513
cut2key <k24lout@gmail.com>
2022-04-04 10:37:27 +0900

    add listing test code

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,14 +8,13 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :item_description, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :quality_id, numericality: { other_than: 1 }
-  validates :ship_fee_id, numericality: { other_than: 1 }
-  validates :country_id, numericality: { other_than: 0 }
-  validates :ship_day_id, numericality: { other_than: 1 }
-  #validates :price, presence: true, format: { with: /\A\d{3,7}+\z/  }
-  validates_inclusion_of :price, in:300..9999999
-  #validates :user, presence: true
+  validates :category_id, numericality: { other_than: 1 ,message: "can't be blank" }
+  validates :quality_id, numericality: { other_than: 1 ,message: "can't be blank" }
+  validates :ship_fee_id, numericality: { other_than: 1 ,message: "can't be blank" }
+  validates :country_id, numericality: { other_than: 0 ,message: "can't be blank" }
+  validates :ship_day_id, numericality: { other_than: 1 ,message: "can't be blank" }
+  validates_inclusion_of :price, in:300..9999999, message: "is invalid"
+  validates :price, numericality: { message: "is invalid" }
 
   belongs_to :user
   has_one_attached :image
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    item_name { Faker::Lorem.characters(number: 8) }
+    item_description {Faker::Lorem.sentence}
+    category_id {2}
+    quality_id {2}
+    ship_fee_id {2}
+    country_id {2}
+    ship_day_id {2}
+    price {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
     
+    after (:build) do |item|
+      item.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
   end
 end
