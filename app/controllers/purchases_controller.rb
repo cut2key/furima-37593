50231e1a5ad45972d6d9f93320f8994a485c0768
cut2key <k24lout@gmail.com>
2022-04-07 08:58:35 +0900

    add purchase base

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,20 @@
 class PurchasesController < ApplicationController
   #before_action :authenticate_user!
   def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    #Address.create(address_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
@@ -8,4 +22,13 @@ class PurchasesController < ApplicationController
   #def purchase_auth
     #if current_user == @item.user
       #redirect_to root_path
+
+  def purchase_params
+    #params.permit(:item_name, :image, :price, :ship_fee_id).merge(user_id: current_user.id, item_id: @item_id)
+    params.require(:purchase_address).permit(:zip_code, :country_id, :city, :st_address, :building, :phone_num).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  #def address_params
+    #params.permit(:zip_code, :country_id, :city, :st_address, :building,:phone_num).merge(purchase_id: @purchase.id)
+  #end
 end
