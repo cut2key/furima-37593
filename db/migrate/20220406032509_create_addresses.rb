1c128988f0830a71dd2511489e572d246caf37fc
cut2key <k24lout@gmail.com>
2022-04-07 08:58:58 +0900

    address base

--- db/migrate/20220406032509_create_addresses.rb
+++ db/migrate/20220406032509_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string :zip_code, null: false
+      t.integer :country_id, null: false
+      t.string :city, null: false
+      t.string :st_address, null: false
+      t.string :building
+      t.string :phone_num, null: false
+      t.references :purchase, null: false, foreign_key: true
       t.timestamps
     end
   end
