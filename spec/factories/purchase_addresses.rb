ffd3a20dd565470cfa54ea71ddc5185782fdb34f
cut2key <k24lout@gmail.com>
2022-04-07 16:27:26 +0900

    test code for purchase

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :purchase_address do
+    transient do
+      address { Gimei.address }
+    end
+    zip_code {'123-4567'}
+    country_id {2}
+    city {address.city.kanji}
+    st_address {'あああ１−１'}
+    #building {}
+    phone_num {Faker::Number.number(digits: 10)}
+    #association :purchase
+  end
+end
