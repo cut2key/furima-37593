7916f2d70363b32e5040894adeff722cf2a19fca
cut2key <k24lout@gmail.com>
2022-04-07 16:27:09 +0900

    some fix

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,15 +2,16 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :zip_code, :country_id, :city, :st_address, :building, :phone_num, :user_id, :item_id
 
-  validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  
   with_options presence: true do
+    validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :country_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :st_address
+    validates :phone_num, format: {with: /\d{10,11}\z/, message: "is invalid"}
     validates :user_id
-    validates :item_id
+    #validates :item_id
   end
-  validates :phone_num, format: {with: /\d{10,11}\z/, message: "is invalid. Delete hyphen(-)"} # with_optionに含めた状態でのエラーメッセージの消し方聞く
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
